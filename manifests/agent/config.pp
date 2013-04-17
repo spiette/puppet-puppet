2ca9eaedefe051c35e72bd32ac1802d19e83c9d4
Simon Piette <simon.piette@savoirfairelinux.com>
2013-04-17 11:34:47 -0400

    created puppet::agent::config

---
+++ manifests/agent/config.pp
@@ -0,0 +1,29 @@
+# === Class: puppet::agent::config
+class puppet::agent::config {
+  # Note that puppet agent or master are restarted if needed
+  concat::fragment {'puppet_main_conf':
+    target  => '/etc/puppet/puppet.conf',
+    content => template('puppet/puppet_main_conf.erb'),
+    order   => '10',
+  } ~>
+  Service <| tag == 'puppetconf' |>
+
+  concat::fragment { 'puppet_certname':
+    target  => '/etc/puppet/puppet.conf',
+    content => "certname = ${puppet::certname}\n",
+    order   => '20',
+  } ~>
+  Service <| tag == 'puppetconf' |>
+
+  concat::fragment { 'puppet_agent_conf':
+    target  => '/etc/puppet/puppet.conf',
+    content => template('puppet/puppet_agent_conf.erb'),
+    order   => '30',
+  } ~>
+  Service <| tag == 'puppet' |>
+
+  file { '/etc/default/puppet':
+    ensure  => present,
+    content => template('puppet/default.erb')
+  }
+}
