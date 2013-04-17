03c836d3442f84ac9c9d94cef76395bc65b28541
Simon Piette <simon.piette@savoirfairelinux.com>
2013-04-17 11:35:27 -0400

    moved config, add anchors

--- manifests/agent.pp
+++ manifests/agent.pp
@@ -5,44 +5,22 @@ class puppet::agent (
   $environment = 'production',
   $certname = $::fqdn,
   ){
+  anchor { 'puppet::agent::begin': }->
   Package['puppet']->
-  Concat::Fragment['puppet_agent_conf', 'puppet_certname']~>
-  Service['puppet']
+  Class['puppet::agent::config']~>
+  Service['puppet'] ->
+  anchor { 'puppet::agent::end': }
+
+  include concat::setup
+  include puppet::agent::config
+
   package { 'puppet':
     ensure => present
   }
-  include concat::setup
 
-  # Note that puppet agent or master are restarted if needed
-  concat::fragment {'puppet_main_conf':
-    target  => '/etc/puppet/puppet.conf',
-    content => template('puppet/puppet_main_conf.erb'),
-    order   => '10',
-  } ->
-  Service <| tag == 'puppetconf' |>
-
-  concat::fragment { 'puppet_certname':
-    target  => '/etc/puppet/puppet.conf',
-    content => "certname = ${certname}\n",
-    order   => '20',
-  } ->
-  Service <| tag == 'puppetconf' |>
-
-  concat::fragment { 'puppet_agent_conf':
-    target  => '/etc/puppet/puppet.conf',
-    content => template('puppet/puppet_agent_conf.erb'),
-    order   => '30',
-  } ->
-  Service <| tag == 'puppet' |>
-
-  file { '/etc/default/puppet':
-    ensure  => present,
-    content => template('puppet/default.erb')
-  }
   service { 'puppet':
     ensure  => running,
     enable  => true,
-    require => File['/etc/default/puppet'],
     tag     => 'puppetconf',
   }
 }
