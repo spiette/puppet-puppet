dc4f98178ab2881665dedd6351866ee89c40cf12
Simon Piette <simon.piette@savoirfairelinux.com>
2013-04-17 19:22:26 -0400

    spec tests for puppet::master::certificate

---
+++ spec/classes/puppet_master_certificate_spec.rb
@@ -0,0 +1,28 @@
+require 'spec_helper'
+
+package = 'puppet'
+service = package
+fqdn = 'agent.domain.local'
+server = 'puppet.domain.local'
+
+describe 'puppet::master::certificate' do
+  let(:title) { 'puppet::certificate' }
+
+  ['Debian', 'RedHat'].each do |osfamily|
+    context "class with some parameters on #{osfamily}" do 
+      let(:facts) { {
+        :osfamily => osfamily,
+        :fqdn => fqdn
+      } }
+
+      it { should create_class('puppet::master::certificate') }
+      #it { should create_class('puppet') }
+      it { should create_exec('puppet-cert-generate')\
+        .with(
+          'command' => /generate.*#{fqdn}/,
+          'creates' => /#{fqdn}.pem/,
+          'timeout' => '180'
+        ) }
+    end
+  end
+end
