8c3d2bec404842391de161565b923dfdd1224d28
Marc-André Courtois <macourtois@iweb.com>
2013-04-19 15:19:08 -0400

    Removed file resource for global.yaml

--- manifests/master/hiera.pp
+++ manifests/master/hiera.pp
@@ -14,12 +14,4 @@ class puppet::master::hiera {
     mode   => '0600',
     source => "puppet:///modules/${module_name}/hiera.yaml",
   }
-
-  file { '/etc/puppet/hieradata/global.yaml':
-    ensure => present,
-    owner  => 'puppet',
-    group  => 'puppet',
-    mode   => '0600',
-    source => "puppet:///modules/${module_name}/global.yaml",
-  }
 }
