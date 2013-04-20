bef12f1742b1c859f86e6019a00605d9777c2166
Simon Piette <piette.simon@gmail.com>
2013-04-20 08:33:23 -0400

    package and hieraconfig spec testing

--- spec/classes/puppet_master_hiera_spec.rb
+++ spec/classes/puppet_master_hiera_spec.rb
@@ -4,29 +4,50 @@ package = 'puppet'
 service = package
 fqdn = 'agent.domain.local'
 server = 'puppet.domain.local'
+    permissions = {
+      :owner => 'puppet',
+      :group => 'puppet',
+      :mode  => '0600'
+    }
 
 describe 'puppet::master::hiera' do
   let(:title) { 'puppet::master::hiera' }
 
-  context "class" do 
+  context "class" do
     let(:facts) { {
       :osfamily => 'Debian',
       :fqdn => fqdn
     } }
 
-    permissions = {
-      :owner => 'puppet',
-      :group => 'puppet',
-      :mode  => '0600'
-    } 
     it { should create_class('puppet::master::hiera') }
-    it { should create_package('hiera') }
+    it { should create_package('hiera')\
+      .with( :ensure => :present) }
     it { should create_file('/etc/puppet/hieradata')\
       .with(permissions.merge({ :ensure => :directory }) ) }
     it { should create_file('/etc/puppet/hiera.yaml')\
       .with(permissions.merge( {
         :ensure => :present,
-        :source => "puppet:///modules/puppet/hiera.yaml"
+        :source => 'puppet:///modules/puppet/hiera.yaml'
         } ) ) }
+  end
+
+  context "class with parameters" do
+    let(:params) { {
+      :ensure => 'latest',
+      :hieraconfig => 'puppet:///files/hiera.yaml'
+    } }
+    let(:facts) { {
+      :osfamily => 'Debian',
+      :fqdn => fqdn
+    } }
+    it { should create_class('puppet::master::hiera') }
+    it { should create_file('/etc/puppet/hieradata')\
+      .with(permissions.merge({ :ensure => :directory }) ) }
+    it { should create_file('/etc/puppet/hiera.yaml')\
+      .with(permissions.merge(
+        { :source => 'puppet:///files/hiera.yaml' }
+      ))}
+    it { should create_package('hiera')\
+      .with( :ensure => :latest) }
   end
 end
