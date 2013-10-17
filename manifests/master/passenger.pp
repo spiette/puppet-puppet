142166c563790a201b482e037658410c6972a858
Simon Piette <simon.piette@savoirfairelinux.com>
2013-10-17 15:04:43 -0400

    setup a vhost for the puppetmaster

--- manifests/master/passenger.pp
+++ manifests/master/passenger.pp
@@ -16,6 +16,37 @@ class puppet::master::passenger {
         source   => "http://${repohost}/${os}/${release}/${release_package}",
       }
       Package['passenger-release']->Package[$puppet::params::passenger_package]
+
+      # Passenger configuration
+      file { '/etc/httpd/conf.d/puppetmaster.conf':
+        ensure  => file,
+        owner   => 'root',
+        group   => 'root',
+        mode    => '0664',
+        content => template('puppet/puppetmaster.conf.erb'),
+        require => File['/usr/share/puppet/rack/puppetmasterd/public'],
+        notify  => Service[$puppet::params::passenger_service],
+      }
+      file { [
+        '/usr/share/puppet/',
+        '/usr/share/puppet/rack',
+        '/usr/share/puppet/rack/puppetmasterd',
+        '/usr/share/puppet/rack/puppetmasterd/public',
+        '/usr/share/puppet/rack/puppetmasterd/tmp',
+      ]:
+        ensure => directory,
+        owner  => 'root',
+        group  => 'root',
+        mode   => '0664',
+      }
+      file { '/usr/share/puppet/rack/puppetmasterd/config.ru':
+        ensure  => file,
+        owner   => 'puppet',
+        group   => 'puppet',
+        mode    => '0664',
+        source  => '/usr/share/puppet/ext/rack/files/config.ru',
+        notify  => Service[$puppet::params::passenger_service],
+      }
     }
     default: {
       fail("Unsupported OS family: ${::osfamily}")
